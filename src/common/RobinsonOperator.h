123811924deb32e4e41221957a3fd38b7ee7ec69
RazorNd <daniltmb@mail.ru>
2016-11-05 20:03:28 +0300

    create RobinsonOperator

---
+++ src/common/RobinsonOperator.h
@@ -0,0 +1,38 @@
+#ifndef ROBINSONOPERATOR_H
+#define ROBINSONOPERATOR_H
+
+#include "SupressOperator.h"
+
+class RobinsonOperator : public SupressOperator
+{
+    enum Direction
+    {
+        ee, // 0
+        ne, // 45
+        nn, // 90
+        nw, // 135
+        ww, // 180
+        sw, // 225
+        ss, // 270
+        se, // 315
+    };
+
+    // SupressOperator interface
+public:
+    virtual PixelsMatrix operator ()(const PixelsMatrix &in) const override;
+private:
+    class GradientCalculator {
+    public:
+        GradientCalculator(const QSize size);
+
+        typedef std::vector<Direction> direction_vector;
+        void operator() (const PixelsMatrix &in) const;
+        PixelsMatrix getGradient() const;
+        direction_vector getGradientDirection() const;
+    private:
+        mutable PixelsMatrix gradient;
+        mutable direction_vector gradientDirection;
+    };
+};
+
+#endif // ROBINSONOPERATOR_H
