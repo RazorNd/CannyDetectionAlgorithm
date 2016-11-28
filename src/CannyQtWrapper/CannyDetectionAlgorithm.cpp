97416f8f19aa014c772fc6a74565a99dcab57a50
RazorNd <daniltmb@mail.ru>
2016-11-28 23:03:29 +0300

    Add functionalyti change suppress operator type

--- src/CannyQtWrapper/CannyDetectionAlgorithm.cpp
+++ src/CannyQtWrapper/CannyDetectionAlgorithm.cpp
@@ -1,15 +1,18 @@
 #include "CannyDetectionAlgorithm.h"
+#include <QDebug>
 
 CannyDetectionAlgorithm::CannyDetectionAlgorithm(QObject *parent) :
     QObject(parent)
 {
     setConnection();
+    setSupressOperator(0);
 }
 
 CannyDetectionAlgorithm::CannyDetectionAlgorithm(int tMin, int tMax, double sigma, QObject *parent) :
     QObject (parent), canny(tMin, tMax, sigma)
 {
     setConnection();
+    setSupressOperator(0);
 }
 
 int CannyDetectionAlgorithm::getTMin() const
@@ -39,6 +42,11 @@ double CannyDetectionAlgorithm::getSigma() const
     return canny.getSigma();
 }
 
+int CannyDetectionAlgorithm::currentSuppressOperatorNumber() const
+{
+    return currentSuppressOperator;
+}
+
 void CannyDetectionAlgorithm::recalculate()
 {
     output = canny(input);
@@ -57,12 +65,28 @@ void CannyDetectionAlgorithm::setSigma(double value)
     emit sigmaChanged(getSigma());
 }
 
+void CannyDetectionAlgorithm::setSupressOperator(int supressOperatorNumber)
+{
+    auto tmp = std::move(suppressOperators[supressOperatorNumber]);
+    qDebug() << tmp.target<PixelsMatrix (const PixelsMatrix &in)>();
+    canny.setSupressOperator(std::move(tmp));
+    suppressOperators[currentSuppressOperatorNumber()] = std::move(tmp);
+    setCurrentSupressOperatorNumber(supressOperatorNumber);
+}
+
+void CannyDetectionAlgorithm::setCurrentSupressOperatorNumber(int suppressOperatorNumber)
+{
+    currentSuppressOperator = suppressOperatorNumber;
+    emit supressOperatorNumberChanged(currentSuppressOperator);
+}
+
 void CannyDetectionAlgorithm::setConnection()
 {
     connect(this, &CannyDetectionAlgorithm::inputImageChanged, &CannyDetectionAlgorithm::recalculate);
     connect(this, &CannyDetectionAlgorithm::tMinChanged, &CannyDetectionAlgorithm::recalculate);
     connect(this, &CannyDetectionAlgorithm::tMaxChanged, &CannyDetectionAlgorithm::recalculate);
     connect(this, &CannyDetectionAlgorithm::sigmaChanged, &CannyDetectionAlgorithm::recalculate);
+    connect(this, &CannyDetectionAlgorithm::supressOperatorNumberChanged, &CannyDetectionAlgorithm::recalculate);
 }
 
 QImage CannyDetectionAlgorithm::getOutput() const
